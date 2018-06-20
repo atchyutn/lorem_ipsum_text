282fc4cc18a9cafb85675ef10622dc868ba96d95
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2018-06-20 15:45:55 +0530

    added paragraph and multiple paragraphs methods

--- lib/lorem_ipsum_text.rb
+++ lib/lorem_ipsum_text.rb
@@ -4,4 +4,16 @@ module LoremIpsumText
   def self.one_line
    "Excepteur ullamco adipisicing nostrud laborum exercitation." 
   end
+
+  def self.paragraph
+    "Minim cillum esse reprehenderit magna. Cupidatat sit ullamco anim dolore aliquip magna sint reprehenderit ex cupidatat. Occaecat mollit esse adipisicing dolor duis. Reprehenderit commodo anim in officia. Duis esse id incididunt exercitation Lorem sunt fugiat. Est ipsum quis nulla veniam culpa enim dolor voluptate aliquip et mollit."
+  end
+
+  def self.multiple_para
+    "Reprehenderit et quis ut aute ea non dolore pariatur laboris cillum nulla. Sit occaecat ipsum nostrud ut nisi eiusmod commodo duis quis. Incididunt nulla aliqua nostrud magna laborum. Non duis sint ipsum ex dolor ea fugiat eiusmod qui. Dolore nostrud in esse est ad pariatur eu aliqua ea velit amet. Eu est veniam nisi non aliqua excepteur magna esse tempor eiusmod sunt nisi. Deserunt cupidatat in ullamco eiusmod ea cupidatat veniam eu Lorem dolore anim exercitation est.
+
+    Adipisicing commodo ex enim dolore mollit ut nisi laboris. Nisi velit sint do proident. Dolore ullamco do non velit enim deserunt. Fugiat minim ullamco do adipisicing do non incididunt magna enim laboris ex ad laboris non. Consequat magna nostrud fugiat tempor voluptate.
+
+    Non aliquip nisi magna velit magna commodo cillum quis cillum ad. Laboris tempor eu laborum excepteur incididunt irure occaecat velit nulla ullamco enim voluptate ex. Id ut consectetur irure anim. Incididunt adipisicing nisi minim elit labore Lorem sunt officia sunt voluptate ipsum. Laboris velit id occaecat irure pariatur reprehenderit culpa. Irure Lorem voluptate elit consequat proident ullamco proident esse quis do dolore ullamco commodo. Sunt occaecat sunt aute ipsum adipisicing."
+  end
 end
